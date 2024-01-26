1d61d97256f13735f3507aa9ef2d612624292a5c
Fourscythegames <Fourscythegames@gmail.com>
2024-01-25 21:17:14 -0800

    More Level
    Levels need balencing
    color background has a bug that when you go past a point and then back under it screws up the darkness and it gets buggy.
    good progress though

--- UpDash/Assets/BackGroundColor.cs
+++ UpDash/Assets/BackGroundColor.cs
@@ -7,13 +7,40 @@ using UnityEngine.UI;
 public class BackGroundColor : MonoBehaviour
 {
     public float startPosition;
+    private float lastPosition;
     public Image bgImage;
-    private int colorIndex = 0;
+    public float colorIndexH = 0;
+    private float colorIndexOutH;
+    public float colorIndexS = 0;
+    private float colorIndexOutS;
+    public float colorIndexV = 0;
+    private float colorIndexOutV;
+    public int totalCycles;
     
-    void Update()
+    void FixedUpdate()
     {
-        var difference = transform.position.y - startPosition;
-        //bgImage.material.color =  UnityEngine.Color.HSVToRGB(colorIndex,48,95);
+        //var difference = (transform.position.y - startPosition)/100;
+        var lastPosDif = (transform.position.y - lastPosition);
+        lastPosition = transform.position.y;
+        
+
+        //print(difference);
+        //print(lastPosDif);
+        colorIndexH = colorIndexH + lastPosDif;
+        if(colorIndexH >= 359){
+            colorIndexH = 0;
+            totalCycles += 1;
+        }else if(colorIndexH <=0 ){
+            colorIndexH = 360;
+            totalCycles -= 1;
+        }
+
+        print(totalCycles);
+        colorIndexOutH = colorIndexH / 360;
+        //print(colorIndexOutH);
+        colorIndexOutS = colorIndexS / 100;
+        colorIndexOutV = (colorIndexV - (15*totalCycles)) / 100;
+        bgImage.material.color =  UnityEngine.Color.HSVToRGB(colorIndexOutH,colorIndexOutS,colorIndexOutV);
         
     }
 }
